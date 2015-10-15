6953e209120cc6e0174e81f745b87e801d5e5bda
yoggy <yoggy0@gmail.com>
2015-10-15 19:47:07 +0900

    Initial Commit

---
+++ src/AccessTestForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AccessTest
+{
+    partial class AccessTest
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textAccdbFilePath = new System.Windows.Forms.TextBox();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonAggregateQuery = new System.Windows.Forms.Button();
+            this.buttonSelectPivot = new System.Windows.Forms.Button();
+            this.buttonCompactDatabase = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDropTable = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textAccdbFilePath
+            // 
+            this.textAccdbFilePath.Location = new System.Drawing.Point(6, 18);
+            this.textAccdbFilePath.Name = "textAccdbFilePath";
+            this.textAccdbFilePath.Size = new System.Drawing.Size(422, 19);
+            this.textAccdbFilePath.TabIndex = 1;
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Location = new System.Drawing.Point(6, 43);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(422, 23);
+            this.buttonChooseFile.TabIndex = 2;
+            this.buttonChooseFile.Text = "既存のaccdbファイルを選択する";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textAccdbFilePath);
+            this.groupBox1.Controls.Add(this.buttonChooseFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(434, 83);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "(1) 操作対象のaccdbファイルを選択してください";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonAggregateQuery);
+            this.groupBox2.Controls.Add(this.buttonSelectPivot);
+            this.groupBox2.Controls.Add(this.buttonCompactDatabase);
+            this.groupBox2.Controls.Add(this.buttonDelete);
+            this.groupBox2.Controls.Add(this.buttonUpdate);
+            this.groupBox2.Controls.Add(this.buttonSelect);
+            this.groupBox2.Controls.Add(this.buttonInsert);
+            this.groupBox2.Controls.Add(this.buttonDropTable);
+            this.groupBox2.Controls.Add(this.buttonCreate);
+            this.groupBox2.Location = new System.Drawing.Point(13, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(433, 365);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "(2) ボタンを押して操作を行ってください";
+            // 
+            // buttonAggregateQuery
+            // 
+            this.buttonAggregateQuery.Location = new System.Drawing.Point(216, 179);
+            this.buttonAggregateQuery.Name = "buttonAggregateQuery";
+            this.buttonAggregateQuery.Size = new System.Drawing.Size(190, 40);
+            this.buttonAggregateQuery.TabIndex = 8;
+            this.buttonAggregateQuery.Text = "Aggregate Query";
+            this.buttonAggregateQuery.UseVisualStyleBackColor = true;
+            this.buttonAggregateQuery.Click += new System.EventHandler(this.buttonAggregateQuery_Click);
+            // 
+            // buttonSelectPivot
+            // 
+            this.buttonSelectPivot.Location = new System.Drawing.Point(216, 225);
+            this.buttonSelectPivot.Name = "buttonSelectPivot";
+            this.buttonSelectPivot.Size = new System.Drawing.Size(190, 40);
+            this.buttonSelectPivot.TabIndex = 7;
+            this.buttonSelectPivot.Text = "PIVOT";
+            this.buttonSelectPivot.UseVisualStyleBackColor = true;
+            this.buttonSelectPivot.Click += new System.EventHandler(this.buttonSelectPivot_Click);
+            // 
+            // buttonCompactDatabase
+            // 
+            this.buttonCompactDatabase.Location = new System.Drawing.Point(20, 120);
+            this.buttonCompactDatabase.Name = "buttonCompactDatabase";
+            this.buttonCompactDatabase.Size = new System.Drawing.Size(386, 40);
+            this.buttonCompactDatabase.TabIndex = 6;
+            this.buttonCompactDatabase.Text = "CompactDatabase";
+            this.buttonCompactDatabase.UseVisualStyleBackColor = true;
+            this.buttonCompactDatabase.Click += new System.EventHandler(this.buttonCompactDatabase_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(20, 317);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(190, 40);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(20, 271);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(190, 40);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "UPDATE";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(20, 225);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(190, 40);
+            this.buttonSelect.TabIndex = 3;
+            this.buttonSelect.Text = "SELECT * FROM ...";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(20, 179);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(190, 40);
+            this.buttonInsert.TabIndex = 2;
+            this.buttonInsert.Text = "INSERT INTO ...";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDropTable
+            // 
+            this.buttonDropTable.Location = new System.Drawing.Point(20, 64);
+            this.buttonDropTable.Name = "buttonDropTable";
+            this.buttonDropTable.Size = new System.Drawing.Size(386, 40);
+            this.buttonDropTable.TabIndex = 1;
+            this.buttonDropTable.Text = "DROP TABLE";
+            this.buttonDropTable.UseVisualStyleBackColor = true;
+            this.buttonDropTable.Click += new System.EventHandler(this.buttonDropTable_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(20, 18);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(386, 40);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "CREATE TABLE";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // AccessTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 475);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AccessTest";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textAccdbFilePath;
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDropTable;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonCompactDatabase;
+        private System.Windows.Forms.Button buttonSelectPivot;
+        private System.Windows.Forms.Button buttonAggregateQuery;
+    }
+}
+
---
+++ src/AccessTestForm.cs
@@ -0,0 +1,332 @@
+﻿//
+//  AccessTestForm.cs - C#からAccessファイル(*.accdb)をSQL文で操作するメモ 
+//
+//  このプログラムを実行する場合はあらかじめ
+//  「Microsoft Access データベース エンジン 2010 再頒布可能コンポーネント」
+//  をインストールしておくこと。
+//
+//    Download Microsoft Access データベース エンジン 2010 再頒布可能コンポーネント from Official Microsoft Download Center
+//    https://www.microsoft.com/ja-jp/download/details.aspx?id=13255
+//
+//    Microsoft Access データベース エンジン 2010 Service Pack (64 ビット版): KB2460011
+//    http://www.microsoft.com/ja-jp/download/details.aspx?id=26605
+//
+//    Microsoft Access データベース エンジン 2010 Service Pack (32 ビット版): KB2460011
+//    http://www.microsoft.com/ja-jp/download/details.aspx?id=26607
+//   
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace AccessTest
+{
+    public partial class AccessTest : Form
+    {
+        public AccessTest()
+        {
+            InitializeComponent();
+            textAccdbFilePath.Text = Properties.Settings.Default.AccdbFilePath;
+        }
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "accdbファイルを選択してください";
+            dlg.RestoreDirectory = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textAccdbFilePath.Text = dlg.FileName;
+                Properties.Settings.Default.AccdbFilePath = dlg.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private OleDbConnection connection;
+        private OleDbCommand command;
+
+        private string AccdbFilePath()
+        {
+            return textAccdbFilePath.Text;
+        }
+
+        private string CreateAccFilePath(string prefix)
+        {
+            string dir = Path.GetDirectoryName(AccdbFilePath());
+            string name = Path.GetFileNameWithoutExtension(AccdbFilePath()); // 拡張子抜きのファイル名
+
+            string filename = dir + @"\" + name + "-" + prefix + ".accdb";
+
+            return filename;
+        }
+
+        private string NewAccdbFilePath()
+        {
+            return CreateAccFilePath("new");
+        }
+
+        private string BackupAccdbFilePath()
+        {
+            return CreateAccFilePath("backup");
+        }
+
+        private void Connect()
+        {
+            if (connection != null) return;
+
+            connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + AccdbFilePath();
+            connection.Open();
+
+            command = new OleDbCommand();
+            command.Connection = connection;
+        }
+
+        private void Disconnect()
+        {
+            if (connection == null) return;
+
+            command.Dispose();
+            command = null;
+
+            connection.Close();
+            connection.Dispose();
+            connection = null;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            command.CommandText = @"CREATE TABLE T_売り上げ(ID COUNTER PRIMARY KEY, 日付 DATETIME, 店舗名 VARCHAR(255) NOT NULL, 商品名 VARCHAR(255) NOT NULL, 個数 INT NOT NULL);";
+            command.ExecuteNonQuery();
+
+            Console.WriteLine(command.CommandText);
+
+            Disconnect();
+        }
+
+        private void buttonDropTable_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            command.CommandText = @"DROP TABLE T_売り上げ;";
+            command.ExecuteNonQuery();
+
+            Console.WriteLine(command.CommandText);
+
+            Disconnect();
+        }
+
+        private void buttonCompactDatabase_Click(object sender, EventArgs e)
+        {
+            //
+            // 参考
+            //   Microsoft Access Compact and Repair using C# .accdb files - Stack Overflow
+            //   http://stackoverflow.com/questions/29201611/microsoft-access-compact-and-repair-using-c-sharp-accdb-files
+            //
+            // "プロジェクト"→"参照の追加"で参照マネージャーを表示
+            // 次に"COM"→"Microsoft Office 14.0 Access Database Engine Object Library"を選択し
+            // 参照を追加するとMicrosoft.Office.Interop.Access.Dao.DBEngineを使えるようになる
+            //
+            var dbengine = new Microsoft.Office.Interop.Access.Dao.DBEngine();
+
+            var src = AccdbFilePath();
+            var dst = NewAccdbFilePath();
+            var bak = BackupAccdbFilePath();
+
+            if (File.Exists(dst))
+            {
+                File.Delete(dst);
+            }
+
+            dbengine.CompactDatabase(src, dst);
+
+            if (File.Exists(bak))
+            {
+                File.Delete(bak);
+            }
+
+            File.Move(src, bak);
+            File.Move(dst, src);
+
+            Console.WriteLine("buttonCompactDatabase_Click");
+        }
+
+        //
+        // INSERT INTOのサンプル
+        //
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            Connect();
+
+            // プリペアドステートメントを使用
+            command.CommandText = @"INSERT INTO T_売り上げ(日付,店舗名,商品名,個数) VALUES(?,?,?,?)";
+            command.Parameters.Add("日付", OleDbType.Date);
+            command.Parameters.Add("店舗名", OleDbType.VarChar);
+            command.Parameters.Add("商品名", OleDbType.VarChar);
+            command.Parameters.Add("個数", OleDbType.Integer);
+
+            for (int i = 0; i < 10000; ++i)
+            {
+                // command.Parameters.Addした順番で配列でアクセス
+                command.Parameters[0].Value = DateTime.Now;
+                command.Parameters[1].Value = "店舗" + String.Format("{0:D8}", r.Next(20) + 1);
+                command.Parameters[2].Value = "商品" + String.Format("{0:D8}", r.Next(20) + 1);
+                command.Parameters[3].Value = r.Next(1000);
+
+                command.ExecuteNonQuery();
+
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine(String.Format("INSERT INTO count={0}", i));
+                }
+            }
+
+            Disconnect();
+        }
+
+        //
+        // SELECT のサンプル
+        //
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            // 店舗00000001の表示
+            command.CommandText = @"SELECT ID,日付,店舗名,商品名,個数 FROM T_売り上げ WHERE 店舗名='店舗00000001'";
+            OleDbDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine(command.CommandText);
+
+            while (reader.Read())
+            {
+                Int32 id = reader.GetInt32(0);
+
+                // nullが入る可能性があるものは、IsDBNull()での確認が必要。ここではnull許容型を使用
+                DateTime? dt = null;
+                if (!reader.IsDBNull(1))
+                {
+                    dt = reader.GetDateTime(1);
+                }
+
+                string shop_name = reader.GetString(2);
+                string product_name = reader.GetString(3);
+                Int32 number = reader.GetInt32(4);
+
+                Console.WriteLine(String.Format("id={0}, 日付={1}, 店舗名={2}, 商品名={3}, 個数={4}", id, dt, shop_name, product_name, number));
+            }
+            command.Dispose();
+
+            // 店舗00000001のカウント表示
+            OleDbCommand tmp = new OleDbCommand(@"SELECT COUNT(ID) FROM T_売り上げ WHERE 店舗名='店舗00000001'", connection);
+            reader = tmp.ExecuteReader();
+            reader.Read();
+            Int32 count = reader.GetInt32(0);
+            Console.WriteLine("COUNT=" + count);
+            tmp.Dispose();
+
+            Disconnect();
+        }
+
+        //
+        // UPDATE のサンプル
+        //
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            // プリペアドステートメントを使用
+            command.CommandText = @"UPDATE T_売り上げ SET 個数=? WHERE 店舗名='店舗00000001'";
+            command.Parameters.Add("個数", OleDbType.Integer).Value = 99999;
+
+            command.ExecuteNonQuery();
+
+            Console.WriteLine(command.CommandText);
+
+            Disconnect();
+        }
+
+        //
+        // DELETE のサンプル
+        //
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            // プリペアドステートメントを使用
+            command.CommandText = @"DELETE FROM T_売り上げ WHERE 店舗名='店舗00000001'";
+            command.ExecuteNonQuery();
+
+            Console.WriteLine(command.CommandText);
+
+            Disconnect();
+        }
+
+        //
+        // 集計クエリのサンプル
+        //
+        private void buttonAggregateQuery_Click(object sender, EventArgs e)
+        {
+            Connect();
+
+            command.CommandText = @"SELECT 店舗名, 商品名, Sum(個数) FROM T_売り上げ GROUP BY 店舗名,商品名 HAVING(店舗名='店舗00000001');";
+            OleDbDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine(command.CommandText);
+
+            while (reader.Read())
+            {
+                string shop_name = reader.GetString(0);
+                string product_name = reader.GetString(1);
+                double total = reader.GetDouble(2); // Sum()はdoubleで帰ってくるみたい
+
+                Console.WriteLine(String.Format("店舗名={0}, 商品名={1}, 個数の合計={2}", shop_name, product_name, total));
+            }
+
+            Disconnect();
+        }
+
+        //
+        // クロス集計クエリのサンプル
+        //
+        //     AccessではSELECT *の結果は255個までという制約がある。
+        //     1列目に項目名が入るため、クロス集計をする際の列数は254が限界なので要注意
+        //     (Accessでクロス集計を作るとQuery結果の2列目に合計が入るので実質253行が限界)
+        //
+        private void buttonSelectPivot_Click(object sender, EventArgs e)
+        {            
+            Connect();
+
+            command.CommandText = @"TRANSFORM Sum(個数) SELECT 店舗名 FROM T_売り上げ GROUP BY 店舗名 PIVOT 商品名";
+
+            OleDbDataReader reader = command.ExecuteReader();
+            Console.WriteLine(command.CommandText);
+
+            // フィールド名の取得
+            Console.WriteLine("    FieldName:");
+            for (int i = 0; i < reader.FieldCount; ++i)
+            {
+                Console.WriteLine(String.Format("        {0}:{1}", i, reader.GetName(i)));
+            }
+
+            while (reader.Read())
+            {
+                Object [] rows = new Object[reader.FieldCount];
+                int count = reader.GetValues(rows);
+
+                for (int i = 0; i < rows.Length; ++i)
+                {
+                    Console.Write(rows[i]);
+                    Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+
+            Disconnect();
+        }
+
+    }
+}
